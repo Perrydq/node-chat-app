4e0a334e41117f2c71c5a92a393bae79a0c1f400
Perry Quinn <perry@missioncontrolinc.com>
2017-02-23 12:07:25 -0800

    add socket.io and connect/disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,20 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 //server public folder
 const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
-app.get('/', (req, res) => {
-    res.status(200).send();
+io.on('connection', (socket) => {
+    console.log('new user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User Disconnected');
+    })
 });
 
-app.listen(port, () => {
-    console.log('server up on port 3000');
-});
 //listen on port 3000
 //console log server up
+server.listen(port, () => {
+    console.log('server up on port 3000');
+});
